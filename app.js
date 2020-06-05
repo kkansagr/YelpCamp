9546da9959a9611f33a4817afeb0ab1e6272c09c
root <khyati.astro@gmail.com>
2020-06-05 16:52:26 +0000

    update app.js

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use("/campgrounds/:id/comments",commentRoutes);
 
 
 
-app.listen(3000,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
 	console.log("Started YelpCamp server...");
 });
